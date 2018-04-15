a34d46d257ebe4090390aab7e8f0fd9ef2299912
shunkaiz <shunkaiz@usc.edu>
2018-04-15 11:49:44 -0700

    get player info in main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import {ShotChart} from "./ShotChart";
+import nba from 'nba'
 export class Main extends React.Component{
+    state = {
+        playerId : nba.findPlayer('Lebron James').playerId,
+        playerInfo : {}
+    }
+
+    componentDidMount(){
+        nba.stats.playerInfo({ PlayerID: this.state.playerId }).then((info)=>{
+           console.log(info);
+           const playerInfo = Object.assign(info.commonPlayerInfo[0], info.playerHeadlineStats[0]);
+           this.setState({playerInfo});
+        });
+
+
+    }
+
     render(){
         const id = 2544;
         return(
 
-            <ShotChart playerId={id}/>
+            <ShotChart playerId={this.state.playerId}/>
         );
     }
 
