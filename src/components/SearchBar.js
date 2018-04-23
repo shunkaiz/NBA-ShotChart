47b412612e1ef917cc54a78c56d0a368ce6992f6
shunkaiz <shunkaiz@usc.edu>
2018-04-22 23:13:08 -0700

    add player image under search bar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { AutoComplete, Input, Icon } from 'antd';
 import nba from 'nba';
-import { PROFILE_PIC_URL_PREFIX } from '../constants';
+import { PEOPLE_PIC_URL_PREFIX } from '../constants';
 
-
+const Option = AutoComplete.Option;
 
 export class SearchBar extends React.Component {
     state = {
@@ -14,8 +14,13 @@ export class SearchBar extends React.Component {
         //console.log(...nba.searchPlayers(value));
         this.setState({
             dataSource: !value ? [] :
-                nba.searchPlayers(value).map( (player) => (player.fullName))
-        });
+                nba.searchPlayers(value).map( ({ fullName, playerId }) =>
+                    <Option key={playerId} value={fullName}>
+                        <img className="player-option-image" src={`${PEOPLE_PIC_URL_PREFIX}/${playerId}.png`}/>
+                        <span className="player-option-label">{fullName}</span>
+                    </Option>
+                    ),
+                });
     }
     onSelect = (value) =>{
         this.props.updatePlayerInfo(value);
