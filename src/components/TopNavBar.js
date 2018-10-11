563a41476dc8348e6105112640af6c848f11a5df
张舜锴 <zhangshunkai@guest-wireless-upc-1601-10-120-024-015.usc.edu>
2018-10-11 13:13:15 -0700

    add login icon

--- src/components/TopNavBar.js
+++ src/components/TopNavBar.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import logo from '../assets/nba-logoman-word-white.svg';
+import {Icon} from 'antd'
 export class TopNavBar extends React.Component{
     render(){
         return(
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
-                <h1 className="App-title"></h1>
+                <div className="App-title">Dashboard</div>
+                <div className="Tool-bar">
+                    <div className="Login-box">
+                        <Icon type="user" theme="outlined" style={{ fontSize: '25px'}}/>
+                    </div>
+                </div>
             </header>
         );
     }
